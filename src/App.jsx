e73b4f15eaf5e0ed966bf16a568b89b517fbd992
Fahad Bhatti <83200973+fahadbhatti2001@users.noreply.github.com>
2024-01-07 23:50:34 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faPhone, faDirections, faMessage } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+
   return (
     <>
       <nav className="flex justify-between items-center py-6 px-16 shadow-md w-full fixed top-0 bg-white">
